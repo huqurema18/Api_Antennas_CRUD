be65154b99f8f0f9da9cad91d197d6069adb851d
huqurema18 <hugo.castiblanco08@gmail.com>
2023-09-24 11:52:04 -0500

    listo para producción

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 from pymongo import MongoClient
+from bson import json_util
 import os
 
 app = Flask(__name__)
@@ -13,25 +14,33 @@ collection = db['StudentsAntenas']  # Reemplazar con el nombre de tu colección
 def add_item():
     data = request.get_json()
     result = collection.insert_one(data)
-    return jsonify({"message": "Item added successfully", "_id": str(result.inserted_id)}), 201
+    return jsonify({"message": "Student added successfully", "_id": str(result.inserted_id)}), 201
 
-@app.route('/items', methods=['GET'])
+
+@app.route('/students', methods=['GET'])
 def get_items():
     items = list(collection.find())
     for item in items:
         item['_id'] = str(item['_id'])  # Convertir ObjectId a str para la serialización JSON
     return jsonify({"items": items}), 200
 
-@app.route('/items/<item_id>', methods=['PUT'])
-def update_item(item_id):
-    data = request.get_json()
-    collection.update_one({"_id": ObjectId(item_id)}, {"$set": data})
-    return jsonify({"message": "Item updated successfully"}), 200
+@app.route('/getStudents/<int:codigo_clase>', methods=['GET'])
+def get_students(codigo_clase):
+    students = collection.find({'codigo_clase': codigo_clase})
+    students_list = [student for student in students]
+    return json_util.dumps(students_list), 200
+
 
 @app.route('/items/<item_id>', methods=['DELETE'])
 def delete_item(item_id):
     collection.delete_one({"_id": ObjectId(item_id)})
     return jsonify({"message": "Item deleted successfully"}), 200
+
+@app.route('/deleteStudents/<int:codigo_clase>', methods=['DELETE'])
+def delete_students(codigo_clase):
+    result = collection.delete_many({'codigo_clase': codigo_clase})
+    return jsonify({'message': f'{result.deleted_count} students deleted'}), 200
+
 
 if __name__ == "__main__":
     app.run(debug=True)
